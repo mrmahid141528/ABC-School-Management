ed55b52926510ecb1654cfbe34fb0f7cd0de6685
mrmahid141528 <info141528@gmail.com>
2026-08-10 23:56:41 +0530

    Add raw try/catch to api/index.php to debug 500 error

--- api/index.php
+++ api/index.php
@@ -1,4 +1,11 @@
 <?php
 // Suppress deprecation warnings caused by newer PHP versions on Vercel
 error_reporting(E_ALL & ~E_DEPRECATED & ~E_USER_DEPRECATED);
-require __DIR__ . '/../public/index.php';
+ini_set('display_errors', '1');
+
+try {
+    require __DIR__ . '/../public/index.php';
+} catch (\Throwable $e) {
+    echo "<h1>CRITICAL FATAL ERROR:</h1>";
+    echo "<pre>" . (string) $e . "</pre>";
+}
