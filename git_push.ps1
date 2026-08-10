001e7002c5296f93272966a46f5348c0f4853a7e
mrmahid141528 <info141528@gmail.com>
2026-08-10 23:05:38 +0530

    Initial commit for Vercel and Supabase

---
+++ git_push.ps1
@@ -0,0 +1,7 @@
+git init
+git add .
+git commit -m "Initial commit for Vercel and Supabase"
+git branch -M main
+git remote remove origin
+git remote add origin https://github.com/mrmahid141528/ABC-School-Management.git
+git push -u origin main
