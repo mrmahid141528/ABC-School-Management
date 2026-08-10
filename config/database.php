5e2503757904d4038548e08d4e1f7c4f718ca7b8
mrmahid141528 <info141528@gmail.com>
2026-08-10 23:38:45 +0530

    Support Vercel Supabase integrated env variables natively

--- config/database.php
+++ config/database.php
@@ -56,11 +56,12 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'url' => env('DATABASE_URL', env('POSTGRES_URL')),
+            'host' => env('DB_HOST', env('POSTGRES_HOST', '127.0.0.1')),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', env('POSTGRES_DATABASE', 'forge')),
+            'username' => env('DB_USERNAME', env('POSTGRES_USER', 'forge')),
+            'password' => env('DB_PASSWORD', env('POSTGRES_PASSWORD', '')),
             'charset' => 'utf8',
             'prefix' => '',
             'schema' => 'public',
