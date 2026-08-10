b0e032afb6a7a18ad931635393ad151a897d4bb6
mrmahid141528 <info141528@gmail.com>
2026-08-10 23:25:43 +0530

    Suppress PHP deprecation warnings on Vercel

--- api/index.php
+++ api/index.php
@@ -1,3 +1,4 @@
 <?php
-
+// Suppress deprecation warnings caused by newer PHP versions on Vercel
+error_reporting(E_ALL & ~E_DEPRECATED & ~E_USER_DEPRECATED);
 require __DIR__ . '/../public/index.php';
