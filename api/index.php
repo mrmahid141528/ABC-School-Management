7c4909b7f543911e33bf90c22fdf56f9e22f19db
mrmahid141528 <info141528@gmail.com>
2026-08-11 00:20:03 +0530

    Use putenv for cache redirects on vercel

--- api/index.php
+++ api/index.php
@@ -5,6 +5,12 @@ ini_set('display_errors', '1');
 
 // Vercel Serverless read-only filesystem fix for Laravel bootstrap cache
 if (isset($_ENV['VERCEL']) || getenv('VERCEL') || true) { // Always override on Vercel entrypoint
+    putenv('APP_SERVICES_CACHE=/tmp/services.php');
+    putenv('APP_PACKAGES_CACHE=/tmp/packages.php');
+    putenv('APP_CONFIG_CACHE=/tmp/config.php');
+    putenv('APP_ROUTES_CACHE=/tmp/routes.php');
+    putenv('APP_EVENTS_CACHE=/tmp/events.php');
+
     $_ENV['APP_SERVICES_CACHE'] = '/tmp/services.php';
     $_ENV['APP_PACKAGES_CACHE'] = '/tmp/packages.php';
     $_ENV['APP_CONFIG_CACHE'] = '/tmp/config.php';
